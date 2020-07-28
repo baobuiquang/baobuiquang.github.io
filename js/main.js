e4c9fea27ceba5e7f734b624328271137c3ffefc
Bui Quang Bao <60503568+buiquangbao@users.noreply.github.com>
2020-07-29 00:30:23 +0700

    update

--- js/main.js
+++ js/main.js
@@ -338,8 +338,3 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
 	});
 });
 
-//----------------------------- Anti Copy Content -------------------------------
-document.addEventListener('copy', function (e) {
-	e.preventDefault();
-	e.clipboardData.setData("text/plain", "Please do not copy the content! If you actually need this for the right purpose, feel free to mail me directly. Thanks! But if you can h4ck this site and copy, please let me know your name! It's my pleasure!");
-})
